835aa3fb972c447f6b127957f8c9325880d5e6a2
obaida2384-afk <obaida2384@gmail.com>
2026-06-08 18:01:04 -0400

    Fix CORS: remove invalid credentials+wildcard combination

--- backend/server.py
+++ backend/server.py
@@ -5941,8 +5941,8 @@ app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_credentials=True,
     allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
